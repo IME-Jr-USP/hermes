a94cc8e41f69452cb2529547652b0ac17ee2a82c
Davi Golebiovski <davigolebiovski@gmail.com>
2026-08-16 01:01:58 -0300

    Refactor discipline search functionality and add logging support

--- backend/buscar_disciplinas.py
+++ backend/buscar_disciplinas.py
@@ -1,26 +1,15 @@
-
 import chromadb
 
-client = chromadb.PersistentClient(path="./chroma_db")
-
-#client = chromadb.Client() # para caso não queira salvar o banco de dados no seu pc
-
-bancos_dados = [banco.name for banco in client.list_collections()] # pega todos os bancos de dados do seu pc e coloca o nome de cada na lista bancos_dados
-
-if not "banco-de-disciplinas" in bancos_dados: # testa se tem o banco de dados no seu pc, se não tiver cria um.
-    banco_disciplinas = client.get_or_create_collection(name="banco-de-disciplinas")
-    for disciplina in disciplinas_instituto:
-        dados = disciplina.obter_dados()
-        banco_disciplinas.add(
-            documents=[
-                dados["nome"]
-            ],
-            ids=[disciplina.sigla] # ids para identificar cada disciplinas.
-        )
-# exemplo de uma das formas de adicionar disciplinas no banco de dados vetoriais do chroma.
+from db import (
+    atualizar_banco_disciplinas,
+    obter_banco_disciplinas,
+    obter_disciplinas_jupiterweb,
+)
 
 
-def formatar_disciplinas(disciplinas_desformatadas: dict):
+def formatar_disciplinas(disciplinas_desformatadas: dict) -> str:
+    """Formata o resultado da busca semântica no banco de dados vetorial para uma string"""
+    
     disciplinas_formatadas = [disciplinas for disciplinas in disciplinas_desformatadas['documents'][0]]
 
     resposta = ""
@@ -29,19 +18,14 @@ def formatar_disciplinas(disciplinas_desformatadas: dict):
         resposta += disciplina_requisitada + "\n"
 
     return resposta
-# função de formatação que retorna apenas o texto da disciplina, apagando todas as outras informações que o query retorna.
 
-
-def buscar_disciplinas(palavra_chave: str, limite: int = 5):
+def buscar_disciplinas(collection: chromadb.Collection, palavra_chave: str, limite: int = 5):
     """
     Dado uma palavra-chave, faz uma busca semântica no banco de dados vetorial
     e traz as x (onde x = limite) disciplinas mais próximas da palavra-chave.
     """
 
-    disciplinas_proximas = banco_disciplinas.query(
+    return collection.query(
         query_texts=[palavra_chave],
         n_results=limite
     )
-    # faz o query no banco de dados que fornecemos para encontrar, por busca semântica, a disciplina que mais se assemelha ao que o usuário quer. (dps vamos mudar o modelo de busca, por enquanto está o padrão do chroma)
-
-    return formatar_disciplinas(disciplinas_proximas)
--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,29 @@
-from buscar_disciplinas import buscar_disciplinas
+import chromadb
 
-def hermes(pergunta):
+from buscar_disciplinas import buscar_disciplinas
+from db import (
+    atualizar_banco_disciplinas,
+    obter_banco_disciplinas,
+    obter_disciplinas_jupiterweb,
+)
+
+
+def hermes(collection: chromadb.Collection, pergunta: str) -> str:
     # Grupo 2 vai preparar o agente aqui, com todos tools necessários.
     # O único tool necessário agora é o buscar_disciplinas, para que o agente
     # possa buscar disciplinas.
     # Importante: instalem o langchain usando uv add langchain
 
-    return buscar_disciplinas(pergunta, limite=3)
+    return buscar_disciplinas(collection, pergunta, limite=3)
     # aqui tem um exemplo já funcional da função de retornar a disciplina mais próxima no banco vetorial
 
 
 if __name__ == "__main__":
+    collection = obter_banco_disciplinas()
+    atualizar_banco_disciplinas(collection, obter_disciplinas_jupiterweb(apenas_oferecidas=True))
+
     while True:
         pergunta = input("Faça uma pergunta para o Hermes: ")
-        resposta = hermes(pergunta)
+        resposta = hermes(collection, pergunta)
 
         print(resposta)
